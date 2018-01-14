d3cdaa6c871f94b892208c88743f7bff77b23eba
ucasxzzzh <591056239@qq.com>
2018-01-14 22:52:04 +0800

    xzhtestresult

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -8,78 +8,78 @@ class CoursesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
- test "should get create" do
+  test "should get create" do
     @course = Course.all.first
     patch :create ,course: { id: 2,name: 'coursename1',open: true }
     assert_response :success
-   end
+  end
 
-   test "should get edit" do
-     get :edit ,id: 2
-      assert_response :success
-   end
+  test "should get edit" do
+    get :edit ,id: 2
+    assert_response :success
+  end
 
 
   test "should get update" do
     @course = Course.all.first
-    patch :update ,id: @course,course: { id: 2,name: 'coursename1',open: true }
-    assert_redirected_to courses_path
-   end
-
-  test "should get destroy" do
-    get :destroy ,id: 2
-    assert_redirected_to courses_path
-   end
-
-  test "should open course" do
-    get :open ,id: 2
+    patch :update ,id: @course,course: {id: 2,name: 'coursename1',open: true }
     assert_redirected_to courses_path
   end
 
-  test "should close course" do
-    get :close ,id: 2
-    assert_redirected_to courses_path
-  end
+test "should get destroy" do
+  get :destroy ,id: 2
+  assert_redirected_to courses_path
+end
+
+test "should open course" do
+  get :open ,id: 2
+  assert_redirected_to courses_path
+end
+
+test "should close course" do
+  get :close ,id: 2
+  assert_redirected_to courses_path
+end
 
 
-  test "should studentlist" do
-    get :studentlist ,id: 2
-    assert_response :success
-  end
-   
-
-  test "should list1" do
-    get :list ,search: '123'
-    assert_response :success
-  end
-   test "should list2" do
-    get :list ,search: '123',status: "所有"
-    assert_response :success
-  end
- test "should list3" do
-    get :list
-    assert_response :success
-  end
- test "should list4" do
-    get :list ,status: "123"
-    assert_response :success
-  end
+test "should studentlist" do
+  get :studentlist ,id: 2
+  assert_response :success
+end
 
 
-  test "should select" do
-    get :select , id: 2
-    assert_redirected_to courses_path
-  end
+test "should list1" do
+  get :list ,search: '123'
+  assert_response :success
+end
+test "should list2" do
+  get :list ,search: '123',status: "所有"
+  assert_response :success
+end
+test "should list3" do
+  get :list
+  assert_response :success
+end
+test "should list4" do
+  get :list ,status: "123"
+  assert_response :success
+end
 
-  test "should coursedetails" do
-    get :coursedetails ,id: 2
-    assert_response :success
-  end
 
-  test "should quit" do
-    get :quit , id: 2
-    assert_redirected_to courses_path
-  end
+test "should select" do
+  get :select , id: 2
+  assert_redirected_to courses_path
+end
+
+test "should coursedetails" do
+  get :coursedetails ,id: 2
+  assert_response :success
+end
+
+test "should quit" do
+  get :quit , id: 2
+  assert_redirected_to courses_path
+end
 
   # test "should get edit" do
   #   get :edit, id: @course
